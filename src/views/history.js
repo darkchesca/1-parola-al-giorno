6a5c41f27e3f1f58fe65dbffcd534004f63a8e59
francescaboe <francesca.boe@phiture.com>
2022-06-02 16:42:51 +0200

    add storage words logic

--- src/views/history.js
+++ src/views/history.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useTranslation} from "react-i18next";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -8,60 +9,22 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import {DeleteOutline} from "@mui/icons-material";
 
-const rows = [
-    {
-        word: 'Parola 1',
-        english: 'Words 1',
-        type: 'verbo',
-    },{
-        word: 'Parola 2',
-        english: 'Words 2',
-        type: 'verbo',
-    },{
-        word: 'Parola 3',
-        english: 'Words 3',
-        type: 'verbo',
-    },{
-        word: 'Parola 4',
-        english: 'Words 3',
-        type: 'verbo',
-    },{
-        word: 'Parola 5',
-        english: 'Words 3',
-        type: 'verbo',
-    },{
-        word: 'Parola 6',
-        english: 'Words 3',
-        type: 'verbo',
-    },{
-        word: 'Parola 7',
-        english: 'Words 3',
-        type: 'verbo',
-    },{
-        word: 'Parola 8',
-        english: 'Words 3',
-        type: 'verbo',
-    },{
-        word: 'Parola 9',
-        english: 'Words 3',
-        type: 'verbo',
-    },
-]
 
 function History(){
+    const { t } = useTranslation();
     const ls = localStorage.wordsHistory ? localStorage.wordsHistory : '[]'
     const localStorageHistory = JSON.parse(ls);
     return (
         <div className="history-container">
             <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-                <TableContainer sx={{ maxHeight: 500 }}>
+                <TableContainer sx={{ maxHeight: 550 }}>
                     <Table stickyHeader aria-label="sticky table">
                         <TableHead>
                             <TableRow>
-                                <TableCell>Word</TableCell>
-                                <TableCell align="right">Translation</TableCell>
-                                <TableCell align="right">Category</TableCell>
-                                <TableCell align="right">Delete</TableCell>
+                                <TableCell>{t('word')}</TableCell>
+                                <TableCell>{t('english')}</TableCell>
+                                <TableCell>{t('category')}</TableCell>
+                                <TableCell align="right">{t('delete')}</TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
@@ -73,9 +36,9 @@ function History(){
                                     <TableCell component="th" scope="row">
                                         {row.original}
                                     </TableCell>
-                                    <TableCell align="right">{row.english}</TableCell>
-                                    <TableCell align="right">{row.type}</TableCell>
-                                    <TableCell align="right"><DeleteOutline/></TableCell>
+                                    <TableCell align="left">{row.english}</TableCell>
+                                    <TableCell align="left">{row.type}</TableCell>
+                                    <TableCell align="right"><DeleteOutline color="error"/></TableCell>
                                 </TableRow>
                             ))}
                         </TableBody>
--- src/views/words.js
+++ src/views/words.js
@@ -9,31 +9,63 @@ const words = require('../words.json');
 
 function Words(){
     const { t } = useTranslation();
-    // show/ hide word card
-    const [showCard, setShowCard] = useState(false);
-    const [word, setWord] = useState({
+    const wordEmptyObj = {
         original:'',
         english: '',
         type: '',
-    });
+    }
+    // show/ hide word card
+    const [showCard, setShowCard] = useState(false);
+    const [type, setType] = useState('')
+    const [word, setWord] = useState(wordEmptyObj);
 
-    // on noun/ verb click pick random word from words array, based on type
-    const onTypeClick = (type) => {
-        const wordsGroup = words[type];
-        let newWord = wordsGroup[Math.floor(Math.random()*wordsGroup.length)];
-        newWord["type"] = type;
-        setWord(newWord);
+    // on type click pick random word from words array, based on type
+    const onTypeClick = (newType) => {
+        setType(newType);
 
-        setShowCard(true)
+        // get filteredWords from localStorage (array with words that have not been saved to history yet)
+        const oldAvailableWords = localStorage.availableWords
+            ? JSON.parse(localStorage.availableWords)
+            : words;
+
+        // isolate array for chosen words group
+        const wordsGroup = oldAvailableWords[newType];
+        // if there are words left in the array
+        if(wordsGroup.length){
+            // pick a random one and show it
+            let newWord = wordsGroup[Math.floor(Math.random()*wordsGroup.length)];
+            newWord["type"] = newType;
+            setWord(newWord);
+            setShowCard(true);
+        } else {
+            // show modal: no words left for this group.
+            alert(t('no_words_left_type', {type}));
+        }
+
     }
 
     // save word to local storage
     const onSaveToHistoryClick = () => {
-        const oldLocalStorage = localStorage.wordsHistory
+        // get localStorage wordsHistory
+        const oldWordsHistory = localStorage.wordsHistory
             ? JSON.parse(localStorage.wordsHistory)
             : [];
-        const wordsHistory = [...oldLocalStorage, word];
+        // add word to history
+        const wordsHistory = [...oldWordsHistory, word];
         localStorage.wordsHistory = JSON.stringify(wordsHistory);
+
+        // get localStorage words array without the words that are in the history
+        let oldAvailableWords = localStorage.availableWords
+            ? JSON.parse(localStorage.availableWords)
+            : words;
+
+        // delete word from array of available words
+        const newAvailableWords = oldAvailableWords[type].filter(w => {
+            return w.original !== word.original
+        });
+        oldAvailableWords[type] = newAvailableWords;
+        localStorage.availableWords = JSON.stringify(oldAvailableWords);
+        setWord(wordEmptyObj);
 
     }
     return (
@@ -73,20 +105,29 @@ function Words(){
                 }}
             >
                 {showCard && <div>
-                    <Card variant="outlined">
-                        <CardContent>
-                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                                {t('how_about')}
-                            </Typography>
-                            <Typography variant="h5" color="text.secondary" component="div">
-                                {word.original}
-                            </Typography>
-                            <Typography variant="h5" color="text.secondary" component="div">
-                                {word.english}
-                            </Typography>
-                        </CardContent>
+                    <Card variant="outlined" sx={{
+                        minHeight:'120px'
+                    }}>
+                        {word.original !== ''
+                            ? <CardContent>
+                                <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
+                                    {t('how_about')}
+                                </Typography>
+                                <Typography variant="h5" color="text.secondary" component="div">
+                                    {word.original}
+                                </Typography>
+                                <Typography variant="h5" color="text.secondary" component="div">
+                                    {word.english}
+                                </Typography>
+                            </CardContent>
+                            : <CardContent>
+                                <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
+                                    {t('word_saved')}
+                                </Typography>
+                        </CardContent>}
                     </Card>
                     <Button
+                        disabled={word.original === ''}
                         variant="contained"
                         color="success"
                         size="sm"
